ee81ef15f34e7df382aeac7f2a4933a16ed71642
Davide Marin <marindavide3@yahoo.it>
2025-08-18 16:41:55 +0200

    Modificata la classe in modo che ogni giorno crei un nuovo file con giorno, mese e anno in modo da avere un file per ogni giorno

--- LoggerHL7.py
+++ LoggerHL7.py
@@ -1,29 +1,49 @@
 import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime
 
-file_handler = TimedRotatingFileHandler(
-    'hl7.log', when='midnight', interval=1, backupCount=7
-)
+"""
+Classe per gestire i logs.
+Nella cartella logs crea ogni giorno un file log denominato: logs_%d%m%Y.log
+"""
+
 class LoggerHL7:
-    def __init__(self, name='HL7Logger', log_file='hl7.log', level=logging.DEBUG):
+    def __init__(self, name='HL7Logger', level=logging.DEBUG):
         logs_dir = 'logs'
-        os.makedirs
-        log_path = os.path.join(logs_dir, log_file)
-        
+        os.makedirs(logs_dir, exist_ok=True)
+
+        # Nome del file log giornaliero
+        log_filename = datetime.now().strftime("logs_%d%m%Y.log")
+        log_path = os.path.join(logs_dir, log_filename)
+
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
 
         if not self.logger.handlers:
             formatter = logging.Formatter(
-                '%(asctime)s [%(levelname)s] %(message)s', 
+                '%(asctime)s [%(levelname)s] %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S'
             )
 
-            file_handler = logging.FileHandler(log_path)
+            # File handler con rotazione giornaliera
+            file_handler = TimedRotatingFileHandler(
+                filename=os.path.join(logs_dir, "logs.log"),  # file "base"
+                when="midnight",
+                interval=1,
+                backupCount=7,
+                encoding="utf-8",
+                utc=False
+            )
+
+            # Sovrascrive il nome fisico dei file rotati con data
+            file_handler.suffix = "%d%m%Y.log"
+            file_handler.extMatch = r"^\d{8}\.log$"
+
             file_handler.setFormatter(formatter)
             self.logger.addHandler(file_handler)
 
+            # Stampa anche a console
             stream_handler = logging.StreamHandler()
             stream_handler.setFormatter(formatter)
             self.logger.addHandler(stream_handler)
@@ -32,7 +52,4 @@ class LoggerHL7:
     def info(self, message): self.logger.info(message)
     def warning(self, message): self.logger.warning(message)
     def error(self, message): self.logger.error(message)
-    def critical(self, message): self.logger.critical(message)   
-    
-        
-        
+    def critical(self, message): self.logger.critical(message)
